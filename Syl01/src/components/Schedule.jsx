312c5f6883d93f7a9d8a060ded88a39f23ed6360
Sylvia <sylvialeja527@hotmail.com>
2024-09-30 17:40:52 -0500

    servicios disponibles

--- Syl01/src/components/Schedule.jsx
+++ Syl01/src/components/Schedule.jsx
@@ -1,13 +1,35 @@
 import React, { useState } from 'react';
 import './Schedule.css';    
-import cattienda from '../components/Categories';
+
+import esg from '../assets/extsoftgel.jpg'; // logo
+import est from '../assets/esmaltado.jpg'; // logo
 
 
 const Schedule = () => {
   
   // Definir las categorias disponibles
-  const [selectedCatt, setselectedCatt] = useState('knail');
+  const [ SelectedSer, setSelectedServ] = useState('extsoftgel');
 
+  const EXTSOFTGEL_SERV= (
+    <div>
+      <p>
+      Las extensiones de uñas con soft gel ofrecen un acabado más natural en comparación con otros métodos. El gel se adapta bien a la forma de la uña, lo que resulta en una apariencia más suave y menos artificial.
+      </p>
+    </div>
+  );
+
+  const SEMIPERMANENTE_SERV= (
+    <div>
+      <p>
+      El gel semipermanente puede durar de dos a tres semanas sin descascararse, lo que es significativamente más largo que el esmalte de uñas tradicional. Esto significa menos tiempo de mantenimiento y una apariencia impecable durante más tiempo      </p>
+    </div>
+  );
+
+  const servi = {
+    extsoftgel: EXTSOFTGEL_SERV,
+    semipermanente: SEMIPERMANENTE_SERV,
+ 
+  };
   return (
     
     <div className="catservicios-container">
@@ -18,16 +40,16 @@ const Schedule = () => {
         {/* Primera fila con imágenes */}
 
         <tr>
-          <td>1</td>
-          <td>2</td>
+          <td><img src={esg} alt="Salon Logo" className="servimagenes" /></td>
+          <td><img src={est} alt="Salon Logo" className="servimagenes" /></td>
 
 
         </tr>
 
         {/* Segunda fila con botones */}
         <tr>    
-          <td> <button className="boton-servicios" onClick={() => setselectedCatt('extsoftgel')}> Extensiones Soft Gel</button> </td>
-          <td> <button className="boton-servicios" onClick={() => setselectedCatt('semipermanente')}>Esmaltado Semipermanente</button></td>
+          <td> <button className="boton-servicios" onClick={() => setSelectedServ('extsoftgel')}> Extensiones Soft Gel</button> </td>
+          <td> <button className="boton-servicios" onClick={() => setSelectedServ('semipermanente')}>Esmaltado Semipermanente</button></td>
 
         </tr>
       </tbody>
@@ -38,7 +60,7 @@ const Schedule = () => {
       <tbody>
       <tr>
             <div className="CATEGORIA-content">
-              <p>{cattienda[selectedCatt]}</p>
+            <p>{servi[SelectedSer]}</p>
             </div>
       </tr>
       </tbody>
--- Syl01/src/components/bancoimagenes.jsx
+++ Syl01/src/components/bancoimagenes.jsx
@@ -25,7 +25,7 @@ const inicialProductos = [
       categoria: 'knail',
     },
     {
-      id: 3, nombre: 'Nail Ice Cube', precio: 100000,
+      id: 3, nombre: 'Nail Ice Cube', precio: 80000,
       descripcion: 'Se caracteriza por su acabado cristalino, transparente o translúcido, que simula la apariencia de hielo. Este estilo tiene un look limpio, fresco y sofisticado, perfecto para mujeres que buscan una estética minimalista pero impactante.',
       imagen: 'https://i5.walmartimages.com/asr/0336c8c5-cc19-4ca0-ade7-8dea427b6159.67ceb887572ff2213aa6a8318ac3baca.jpeg?odnHeight=768&odnWidth=768&odnBg=FFFFFF',
       imagenes: [
@@ -36,7 +36,7 @@ const inicialProductos = [
       categoria: 'knail',
     },
     {
-      id: 4, nombre: 'Tintas', precio: 100000,
+      id: 4, nombre: 'Tintas', precio: 80000,
       descripcion: 'expresa tu creatividad y personalidad a través del nail art. ',
       imagen: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRrlwqDPVsquA10Y13tVvyHSIDEGW5TujPSMCAQ3p-i_zFgzTTa9hlnGFv8r5mYoWDCgkQ&usqp=CAU',
       imagenes: [
@@ -89,6 +89,100 @@ const inicialProductos = [
         'https://i.pinimg.com/736x/82/99/33/829933f8b6d976c3c5d3cf79e2f70eda.jpg',
         'https://i.pinimg.com/564x/1b/f5/5c/1bf55c84e145f0db8111db44420b8625.jpg',
         'https://i.pinimg.com/564x/8b/d8/11/8bd811d3c83464e0049cae242784fd03.jpg'
+      ],
+      categoria: 'anime',
+    },
+
+
+    {
+      id: 9, nombre: 'Flores', precio: 90000,
+      descripcion: '.',
+      imagen: 'https://i.pinimg.com/564x/45/46/67/454667751b16fcd8f5566f47250c06a0.jpg',
+      imagenes: [
+        'https://i.pinimg.com/564x/1d/be/25/1dbe25fd8424428ba8dc359b60867bec.jpg',
+        'https://i.pinimg.com/564x/a1/c9/a5/a1c9a5937293352c8fecb71e4f580a6c.jpg',
+        'https://i.pinimg.com/564x/dd/16/c6/dd16c6b608b9f04cddbc58852553b003.jpg'
+      ],
+      categoria: 'tresd',
+    },
+    {
+      id: 10, nombre: 'Cute', precio: 100000,
+      descripcion: '.',
+      imagen: 'https://i.pinimg.com/564x/b0/fd/89/b0fd8945c6640a92da8dc9b26d30e8e3.jpg',
+      imagenes: [
+        'https://i.pinimg.com/564x/4d/38/41/4d3841e4af722c0b2b033840c636faf7.jpg',
+        'https://i.pinimg.com/564x/d6/5a/e3/d65ae314f96d42d4d9e978742fc74943.jpg',
+        'https://i.pinimg.com/564x/68/f7/82/68f78261d8670e972f2741e2e6965b34.jpg',
+      ],
+      categoria: 'tresd',
+    },
+    {
+      id: 11, nombre: 'Charms', precio: 70000,
+      descripcion: '.',
+      imagen: 'https://i.pinimg.com/564x/e5/45/5e/e5455e085b8b77fb328bfef9367e31d7.jpg',
+      imagenes: [
+        'https://i.pinimg.com/736x/f9/ae/74/f9ae74e0422758fc3c4748b08acf7c50.jpg',
+        'https://i.pinimg.com/564x/1e/f8/e0/1ef8e0f11fa50c1f28ae1bd198eb57f4.jpg',
+        'https://i.pinimg.com/736x/91/47/12/914712ee6837a25a00b921c8cedd350a.jpg'
+      ],
+      categoria: 'tresd',
+    },
+  
+    {
+      id: 12, nombre: 'Concha Nacar', precio: 100000,
+      descripcion: '.',
+      imagen: 'https://i.pinimg.com/564x/24/11/2b/24112b1fddfd63631e3b1d9a62e1aa36.jpg',
+      imagenes: [
+        'https://i.pinimg.com/564x/a7/b7/85/a7b7854cf9c3956ee3b21db98eaa10cd.jpg',
+        'https://i.pinimg.com/564x/ae/b5/a3/aeb5a3768086f301bafdb40db554d598.jpg',
+        'https://i.pinimg.com/564x/d1/f0/b4/d1f0b40fb331834cd7b2ab3add736e8b.jpg',
+      ],
+      categoria: 'tendencia',
+    },
+    {
+      id: 13, nombre: 'Paisajes', precio: 120000,
+      descripcion: '.',
+      imagen: 'https://i.pinimg.com/564x/ee/3a/50/ee3a504f1564344b2eb96ca3ac24141e.jpg',
+      imagenes: [
+        'https://i.pinimg.com/736x/ca/54/a0/ca54a0f7a74541c50a340bc08d7620b8.jpg',
+        'https://i.pinimg.com/564x/40/ee/cc/40eecc4b504c40e7e3ecba2a65049ebc.jpg',
+        'https://i.pinimg.com/736x/db/15/20/db1520c6513ccef1c258a4a7d3813e4c.jpg'
+      ],
+      categoria: 'tendencia',
+    },
+  
+    {
+      id: 14, nombre: 'Aceite para cuticula', precio: 15000,
+      descripcion: '.',
+      imagen: 'https://i.pinimg.com/564x/e4/c2/f9/e4c2f9695daffa8cade6f9e8ce099584.jpg',
+      imagenes: [
+        'https://i.pinimg.com/564x/0d/a0/6e/0da06ec1143bb52f6ed89091ef034e5c.jpg',
+        'https://i.pinimg.com/564x/e7/6a/cb/e76acb9857168ef96e2431347b0e653b.jpg',
+        'https://i.pinimg.com/564x/0c/1b/17/0c1b17f49b539b1d88f8680f100a18b2.jpg',
+      ],
+      categoria: 'accesorios',
+    },
+
+    {
+      id: 15, nombre: 'Juji ito', precio: 130000,
+      descripcion: '.',
+      imagen: 'https://i.pinimg.com/564x/8a/53/65/8a536537c0109446cd9735577db21e66.jpg',
+      imagenes: [
+        'https://i.pinimg.com/564x/69/25/9b/69259b75b1ddb746da77d757c299f1d7.jpg',
+        'https://i.pinimg.com/564x/e7/6a/cb/e76acb9857168ef96e2431347b0e653b.jpg',
+        'https://i.pinimg.com/736x/4d/1f/16/4d1f160d3b22c1f58e1b77a818f3bb3c.jpg',
+      ],
+      categoria: 'anime',
+    },
+
+    {
+      id: 16, nombre: 'Varios', precio: 130000,
+      descripcion: '.',
+      imagen: 'https://i.pinimg.com/564x/bc/dc/4b/bcdc4b46510f0c3331ed165e0647b788.jpg',
+      imagenes: [
+        'https://i.pinimg.com/564x/e5/18/dd/e518ddd8f525763ca07c964c2eb3d703.jpg',
+        'https://i.pinimg.com/564x/38/73/7f/38737fda673eb26cf52b576bfefbe1d7.jpg',
+        'https://i.pinimg.com/564x/d0/29/b7/d029b7dcfc654480cad1c9492fb68073.jpg',
       ],
       categoria: 'anime',
     },
