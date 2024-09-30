f381332ceabbc6c1585910ef3b9f56b916217e65
Sylvia <sylvialeja527@hotmail.com>
2024-09-30 17:55:21 -0500

    imagenes en pestaña de dudas

--- Syl01/src/components/Faq.jsx
+++ Syl01/src/components/Faq.jsx
@@ -1,11 +1,33 @@
 import React from 'react';
-
+import './Home.css'; // Asegúrate de que la ruta sea correcta
+import paso1 from '../assets/paso1.jpg'; // logo
+import paso2 from '../assets/paso2.jpg'; // logo
+import paso3 from '../assets/paso3.jpg'; // logo
 function Faq() {
-  return (
-    <div>
-      <h1>DUDAS</h1>
-     </div>
-  );
-}
+ 
+
+  
+    return (
+  
+        <div className="table-containerdescuentos">
+        <table className="my-tabledescuentos"> 
+          <tbody>
+            <tr>
+               <img src={paso1} alt="Salon Logo"   />
+              <img src={paso2} alt="Salon Logo"   /> 
+              <img src={paso3} alt="Salon Logo"   /> 
+            </tr>
+             
+           
+             </tbody>
+        </table>
+        
+        {/* Otros contenidos de tu página */}
+      </div>
+    );
+  };
+  
+
+
 
 export default Faq;
